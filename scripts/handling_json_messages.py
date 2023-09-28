fe5c2f368e0049ff5fb0cddd56d0ef80d24cc175
ProgrammingOperative <wachura11t@gmail.com>
2023-09-28 17:00:52 +0300

    update lambda for image processing

--- scripts/handling_json_messages.py
+++ scripts/handling_json_messages.py
@@ -8,7 +8,7 @@ from time import sleep
 KAFKA_TOPIC = "check_kafka_ingest"
 
 # Initialize Kafka producer
-producer = KafkaProducer(bootstrap_servers=['18.119.19.35:9092'])
+producer = KafkaProducer(bootstrap_servers=['18.119.19.35:9092'], value_serializer=lambda m: json.dumps(m).encode('utf-8'))
 
 # Initialize S3 client
 s3 = boto3.client('s3')
@@ -22,17 +22,15 @@ def lambda_handler(event, context):
         # Construct the S3 object URL
         s3_object_url = f"s3://{bucket_name}/{object_key}"
 
-
-
-        response = s3.get_object(Bucket=bucket_name, Key=object_key)
-        image_object = response['Body'].read()
-
         # Create a message to send to Kafka
         kafka_message = {
             "s3_object_url": s3_object_url,
-            "image_object": image_object
+            "message": record['eventName']
         }
 
+
+        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+        json_data = response['Body'].read().decode('utf-8')
 
         # Send the message to Kafka
         producer.send(KAFKA_TOPIC, value=kafka_message)
